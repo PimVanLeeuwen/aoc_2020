52ae6a63749c0c5b1595ee7ce89cc5080e89fcf8
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-01 16:12:11 +0100

    Day 1 completed, nothing fancy just some for loops which still ran within a second.

--- day_1/Day1.cpp
+++ day_1/Day1.cpp
@@ -5,24 +5,31 @@
 #include <ostream>
 #include <sstream>
 #include <vector>
+#include <set>
 using namespace std;
 
 void Day1::execute(const vector<string>& lines) {
     long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    set<int> numbers;
 
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+    for (const auto &line: lines) numbers.insert(stoi(line));
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
-
-        matrix.push_back(matrix_line);
+    for (const auto &n1 : numbers) {
+          for (const auto &n2 : numbers) {
+              if (n1 + n2 == 2020) {part_1 = n1*n2; break;}
+          }
     }
 
     cout << "Part 1: " << part_1 << endl;
+
+    for (const auto &n1 : numbers) {
+        for (const auto &n2 : numbers) {
+            for (const auto &n3 : numbers) {
+                if (n1 + n2 + n3 == 2020) {part_2 = n1*n2*n3; break;}
+            }
+        }
+    }
+
     cout << "Part 2: " << part_2 << endl;
 }
 
