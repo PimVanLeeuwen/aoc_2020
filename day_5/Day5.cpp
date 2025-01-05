ba36899656367b423c896c35d91e8b8db94a2073
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-06 00:15:36 +0100

    Day 5 completed.

--- day_5/Day5.cpp
+++ day_5/Day5.cpp
@@ -6,24 +6,41 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 void Day5::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    int part_1 = 0;
+    set<int> taken;
 
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+        string row = line.substr(0, 7);
+        string seat = line.substr(7, 3);
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+        int row_low = 0, row_high = 127;
+        int seat_low = 0, seat_high = 7;
 
-        matrix.push_back(matrix_line);
+        for (const char &r : row) {
+            const int half = (row_high - row_low + 1)/2;
+            (r == 'B') ? row_low += half : row_high -= half;
+        }
+
+        for (const char &s : seat) {
+            const int half = (seat_high - seat_low + 1)/2;
+            (s == 'R') ? seat_low += half : seat_high -= half;
+        }
+
+        part_1 = max(part_1, row_low*8 + seat_low);
+
+        taken.insert(row_low*8 + seat_low);
     }
 
     cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+
+    int prev = -1;
+    for (const int i : taken) {
+        if (i == prev + 2) cout << "Part 2: " << i-1 << endl;
+        prev = i;
+    }
 }
