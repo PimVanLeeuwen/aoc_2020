9fe7ac9e7e992c2a4082f092a8af79110e0cd02a
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-06 00:24:31 +0100

    Day 6 completed.

--- day_6/Day6.cpp
+++ day_6/Day6.cpp
@@ -10,18 +10,24 @@ using namespace std;
 void Day6::execute(const vector<string> &lines) {
 
     long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    set<char> yes;
+    map<char, int> yes_map;
+    int group_size = 0;
 
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+        if (line.empty()) {
+            part_1 += static_cast<long>(yes.size());
+            for (const auto [c, l] : yes_map) if (l == group_size) part_2++;
+            yes.clear(); yes_map.clear(); group_size = 0; continue;
+        }
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+        for (const auto &c: line) { yes.insert(c); yes_map[c]++; }
 
-        matrix.push_back(matrix_line);
+        group_size++;
     }
+
+    part_1 += static_cast<long>(yes.size());
+    for (const auto [c, l] : yes_map) if (l == group_size) part_2++;
 
     cout << "Part 1: " << part_1 << endl;
     cout << "Part 2: " << part_2 << endl;
