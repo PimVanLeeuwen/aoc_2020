c81dd860001b0bcace2d03ee1a76484b98ea88df
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-07 22:00:38 +0100

    Completed day 8

--- day_8/Day8.cpp
+++ day_8/Day8.cpp
@@ -9,9 +9,28 @@
 
 using namespace std;
 
+pair<bool, long> terminates(const vector<tuple<string, char, int>>& matrix) {
+    set<long> executed_pointers;
+    long pointer = 0, accumulator = 0;
+
+    while (executed_pointers.find(pointer) == executed_pointers.end()) {
+        if (pointer == matrix.size()) return make_pair(true, accumulator);
+        executed_pointers.insert(pointer);
+        if (get<0>(matrix[pointer]) == "nop") {
+            pointer++;
+        } else if (get<0>(matrix[pointer]) == "acc") {
+            get<1>(matrix[pointer]) == '+' ? accumulator += get<2>(matrix[pointer]) : accumulator -= get<2>(matrix[pointer]);
+            pointer++;
+        } else {
+            get<1>(matrix[pointer]) == '+' ? pointer += get<2>(matrix[pointer]) : pointer -= get<2>(matrix[pointer]);
+        }
+    }
+
+    return make_pair(false, accumulator);
+}
+
 void Day8::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    vector<tuple<string, char, int>> matrix;
 
     for (const auto &line: lines) {
         // convert into a string stream to split on token
@@ -21,9 +40,17 @@ void Day8::execute(const vector<string> &lines) {
         // separate line
         while (getline(ss, token, ' ')) matrix_line.push_back(token);
 
-        matrix.push_back(matrix_line);
+        matrix.emplace_back(matrix_line[0], matrix_line[1][0],
+            stoi(matrix_line[1].substr(1, matrix_line[1].length() - 1)));
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+    cout << "Part 1: " << terminates(matrix).second << endl;
+
+    for (int i = 0; i < matrix.size(); i++) {
+        if (get<0>(matrix[i]) == "acc") continue;
+
+        get<0>(matrix[i]) == "nop" ? get<0>(matrix[i]) = "jmp" : get<0>(matrix[i]) = "nop";
+        if (terminates(matrix).first) cout << "Part 2: " << terminates(matrix).second << endl;
+        get<0>(matrix[i]) == "nop" ? get<0>(matrix[i]) = "jmp" : get<0>(matrix[i]) = "nop";
+    }
 }
