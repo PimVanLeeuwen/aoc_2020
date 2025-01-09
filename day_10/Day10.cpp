a22c69d54f5df7a5a707a63c7853ef4b3eb00cc0
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-09 20:17:39 +0100

    Completed day 10

--- day_10/Day10.cpp
+++ day_10/Day10.cpp
@@ -11,20 +11,26 @@
 using namespace std;
 
 void Day10::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    vector adapters = {0};
 
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+    for (const auto &line: lines) adapters.push_back(stoi(line));
+    sort(adapters.begin(), adapters.end());
+    adapters.push_back(adapters.back()+3);
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
-
-        matrix.push_back(matrix_line);
+    pair diffs = {0, 0};
+    for (int i = 0; i < adapters.size(); i++) {
+        if (i > 0 && adapters[i] - adapters[i-1] == 1) diffs.first++;
+        if (i > 0 && adapters[i] - adapters[i-1] == 3) diffs.second++;
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+    cout << "Part 1: " << diffs.first*diffs.second << endl;
+
+    map<long, long> ways_to_reach = {{0,1}};
+
+    for (const int jolt : adapters) {
+        if (jolt == 0) continue;
+        for (int i = jolt - 1; i >= jolt - 3; i--) ways_to_reach[jolt] += ways_to_reach[i];
+    }
+
+    cout << "Part 2: " << ways_to_reach[adapters.back()] << endl;
 }
