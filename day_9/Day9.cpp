e3fb466a1a5969850ea065accb47bc38220e1884
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-09 19:53:02 +0100

    Completed day 9

--- day_9/Day9.cpp
+++ day_9/Day9.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <deque>
 #include <iostream>
+#include <numeric>
 #include <sstream>
 #include <ostream>
 #include <set>
@@ -11,22 +12,46 @@
 using namespace std;
 
 void Day9::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    long part_1 = 0;
+    deque<long> numbers;
+    vector<long> numbers_list;
 
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+        if (numbers.size() < 25) numbers.push_back(stol(line));
+        else {
+            bool found = false;
+            for (int i = 0; i < 25; i++) {
+                for (int j = i + 1; j < 25; j++) if (numbers[i] + numbers[j] == stol(line)) { found = true; break; }
+                if (found) break;
+            }
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+            if (!found) {
+                part_1 = stoi(line); cout << "Part 1: " << part_1 << endl;
+                break;
+            }
 
-        matrix.push_back(matrix_line);
+            numbers.push_back(stol(line)); numbers.pop_front();
+        }
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+    for (const auto &line: lines) numbers_list.push_back(stol(line));
+
+    int left = 0;
+    int right = 1;
+
+    vector sum_list(numbers_list.begin() + left, numbers_list.begin() + right);
+    long sum = accumulate(sum_list.begin(), sum_list.end(), 0);
+
+    while (sum != part_1) {
+        if (sum > part_1) left++;
+        else right++;
+
+        sum_list = vector<long>(numbers_list.begin() + left, numbers_list.begin() + right);
+        sum = accumulate(sum_list.begin(), sum_list.end(), 0);
+    }
+
+
+    cout << "Part 2: " << *min_element(sum_list.begin(), sum_list.end()) + *max_element(sum_list.begin(), sum_list.end()) << endl;
 }
 
 
