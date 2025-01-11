f5d1c3d72ba48d8d7d0f6ea1ecb040eb99cf8dd0
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-11 21:32:43 +0100

    Completed day 12

--- day_12/Day12.cpp
+++ day_12/Day12.cpp
@@ -8,21 +8,57 @@
 
 using namespace std;
 
+map<char, pair<int, int>> directions = {
+{'E', {0,1}},
+{'W', {0,-1}},
+{'N', {-1,0}},
+{'S', {1,0}},
+};
+
 void Day12::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
     vector<vector<string>> matrix;
 
+    tuple ship = {0, 0, 'E'};
+    tuple ship_2 = {0, 0};
+    tuple waypoint = {-1, 10};
+
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+        const int value = stoi(line.substr(1, line.size()-1));
+        if (line[0] == 'F') {
+            get<0>(ship) += directions[get<2>(ship)].first*value; get<1>(ship) += directions[get<2>(ship)].second*value;
+            get<0>(ship_2) += get<0>(waypoint)*value; get<1>(ship_2) += get<1>(waypoint)*value;
+        } else if (line[0] == 'L') {
+            int turns = value/90;
+            while (turns--) {
+                if (get<2>(ship) == 'E') get<2>(ship) = 'N';
+                else if (get<2>(ship) == 'N') get<2>(ship) = 'W';
+                else if (get<2>(ship) == 'W') get<2>(ship) = 'S';
+                else if (get<2>(ship) == 'S') get<2>(ship) = 'E';
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+                const int left = get<0>(waypoint);
+                get<0>(waypoint) = get<1>(waypoint)*-1;
+                get<1>(waypoint) = left;
+            }
+        } else if (line[0] == 'R') {
+            int turns = value/90;
+            while (turns--) {
+                if (get<2>(ship) == 'E') get<2>(ship) = 'S';
+                else if (get<2>(ship) == 'N') get<2>(ship) = 'E';
+                else if (get<2>(ship) == 'W') get<2>(ship) = 'N';
+                else if (get<2>(ship) == 'S') get<2>(ship) = 'W';
 
-        matrix.push_back(matrix_line);
+                const int left = get<0>(waypoint);
+                get<0>(waypoint) = get<1>(waypoint);
+                get<1>(waypoint) = -1*left;
+            }
+        }
+        else if (line[0] == 'N') {get<0>(ship) -= value; get<0>(waypoint) -= value;}
+        else if (line[0] == 'E') {get<1>(ship) += value; get<1>(waypoint) += value;}
+        else if (line[0] == 'S') {get<0>(ship) += value; get<0>(waypoint) += value;}
+        else if (line[0] == 'W') {get<1>(ship) -= value; get<1>(waypoint) -= value;}
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+
+    cout << "Part 1: " << abs(get<0>(ship)) + abs(get<1>(ship)) << endl;
+    cout << "Part 2: " << abs(get<0>(ship_2)) + abs(get<1>(ship_2)) << endl;
 }
