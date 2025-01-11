629a723eed9a4d92b036bfe3b23e0ea78b9a121c
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-11 22:33:30 +0100

    Completed day 13

--- day_13/Day13.cpp
+++ day_13/Day13.cpp
@@ -8,21 +8,49 @@
 
 using namespace std;
 
+// (num + -10) % 41 == 0
+// (num + 0) % 521 == 0
+// (num + 8) % 23 == 0
+// (num + 31) % 523 == 0
+// (num + 44) % 13 == 0
+bool satisfies(const long n) {
+    if ((n - 10) % 41 != 0) return false;
+    if (n % 521 != 0) return false;
+    if ((n + 8) % 23 != 0) return false;
+    if ((n + 31) % 523 != 0) return false;
+    if ((n + 44) % 13 != 0) return false;
+
+    return true;
+}
+
 void Day13::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    const int time = stoi(lines.front());
 
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+    stringstream ss(lines.back()); string token;
+    int bus = -1; int time_wait = -1; int index = 0;
+    vector<pair<int, int>> busses;
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+    // separate line
+    while (getline(ss, token, ',')) {
+        if (token != "x" && bus == -1) { bus = stoi(token); time_wait = bus - (time % bus); }
+        else if (token != "x" && stoi(token) - (time % stoi(token)) < time_wait) { bus = stoi(token); time_wait = bus - (time % bus); }
 
-        matrix.push_back(matrix_line);
+        if (token != "x") busses.emplace_back(stoi(token),index);
+        index++;
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+    for (const auto [bus, index] : busses) {
+        cout << "(num + " << index - 19 << ") % " << bus << " == 0" << endl;
+    }
+
+    // (num + -19) % 19 == 0
+    // (num + 17) % 17 == 0
+    // (num + 29) % 29 == 0
+    // (num + 37) % 37 == 0
+    long multiple = 19 * 17 * 29 * 37;
+
+    while (!satisfies(multiple)) multiple += 19 * 17 * 29 * 37;
+
+    cout << "Part 1: " << bus*time_wait << endl;
+    cout << "Part 2: " << multiple - 19 << endl;
 }
