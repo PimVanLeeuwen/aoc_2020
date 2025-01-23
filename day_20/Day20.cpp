6e54b5b5187afcfaf35a6bc7d6b2306308e50e2c
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-23 21:05:41 +0100

    Started on day 20, not yet done

--- day_20/Day20.cpp
+++ day_20/Day20.cpp
@@ -1,6 +1,7 @@
 #include "Day20.h"
 
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <ostream>
@@ -8,20 +9,49 @@
 
 using namespace std;
 
+struct tile {
+    int id;
+    vector<vector<char>> square_chars;
+};
+
+
 void Day20::execute(const vector<string> &lines) {
     long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    vector<tile*> images;
+
+    int number = -1;
+    vector<vector<char>> image;
 
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
-
-        matrix.push_back(matrix_line);
+        if (line.find("Tile") != string::npos) {
+            number = stoi(line.substr(line.find(" " + 1, 4)));
+        } else if (line.empty()) {
+            images.emplace_back(new tile());
+            images.back()->id = number;
+            images.back()->square_chars = image;
+            number = -1;
+            image.clear();
+        } else {
+            vector<char> image_line;
+            for (char c : line) image_line.push_back(c);
+            image.push_back(image_line);
+        }
     }
+
+    if (number != -1) {
+        images.emplace_back(new tile());
+        images.back()->id = number;
+        images.back()->square_chars = image;
+    }
+
+    cout << images.size() << endl;
+
+    if (floor(sqrt(images.size()) != ceil(sqrt(images.size())))) throw invalid_argument("number not a square");
+
+    int image_size = static_cast<int>(sqrt(images.size()));
+
+
 
     cout << "Part 1: " << part_1 << endl;
     cout << "Part 2: " << part_2 << endl;
