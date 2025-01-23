eccac29a40f1a98792f022a1cc8b6e276febf6c9
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-23 13:24:23 +0100

    Completed day 18

--- day_18/Day18.cpp
+++ day_18/Day18.cpp
@@ -5,22 +5,56 @@
 #include <map>
 #include <ostream>
 #include <sstream>
+#include <regex>
 
 using namespace std;
 
+struct N {
+    N(const long v) : v(v) {}
+    long v;
+};
+
+N operator+(const N a, const N b) {
+    return a.v + b.v;
+}
+
+N operator-(const N a, const N b) {
+    return a.v * b.v;
+}
+
+N operator/(const N a, const N b) {
+    return a.v + b.v;
+}
+
 void Day18::execute(const vector<string> &lines) {
     long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    vector<N> values_2 = {
+        // insert values from cout here
+    };
+    vector<N> values = {
+        // insert values from cout here
+    };
 
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+    for (string line: lines) {
+        regex number_regex("\\d+");
+        line = regex_replace(line, number_regex, "N($&)");
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+        regex mul_regex("\\*");
+        line = regex_replace(line, mul_regex, "-");
 
-        matrix.push_back(matrix_line);
+        //this for part 2 only
+        regex add_regex("\\+");
+        line = regex_replace(line, add_regex, "-");
+
+        cout << line << "," << endl;
+    }
+
+    for (auto val : values) {
+        part_1 += val.v;
+    }
+
+    for (auto val : values_2) {
+        part_2 += val.v;
     }
 
     cout << "Part 1: " << part_1 << endl;
