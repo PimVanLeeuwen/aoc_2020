01789bbea67088dc082cbafe4afe07fcf553974f
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-28 21:52:53 +0100

    Completed day 24, part 1

--- day_24/Day24.cpp
+++ day_24/Day24.cpp
@@ -4,23 +4,59 @@
 #include <iostream>
 #include <map>
 #include <ostream>
+#include <set>
 #include <sstream>
 
 using namespace std;
 
 void Day24::execute(const vector<string> &lines) {
     long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    vector tiles_grid(400, vector(400, false));
+
+    set<pair<int, int>> tiles_flipped;
 
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+        pair<int, int> tile = {200,200};
+        int i = 0;
+        while (i < line.size()) {
+            if (line[i] == 'e') {
+                tile.first++;
+                i++;
+            } else if (line[i] == 'w') {
+                tile.first--;
+                i++;
+            } else if (line[i] == 's' && line[i + 1] == 'w') {
+                tile.first--;
+                tile.second++;
+                i += 2;
+            } else if (line[i] == 'n' && line[i + 1] == 'e') {
+                tile.first++;
+                tile.second--;
+                i += 2;
+            } else if (line[i] == 's' && line[i + 1] == 'e') {
+                tile.second++;
+                i += 2;
+            } else if (line[i] == 'n' && line[i + 1] == 'w') {
+                tile.second--;
+                i += 2;
+            }
+        }
+        tiles_grid[tile.second][tile.first] = tiles_grid[tile.second][tile.first] ? false : true;
+    }
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
+    for (int y = 0; y < tiles_grid.size(); y++) {
+        for (int x = 0; x < tiles_grid.size(); x++) {
+            if (tiles_grid[y][x]) part_1++;
+        }
+    }
 
-        matrix.push_back(matrix_line);
+    for (int d = 0; d < 100; d++) {
+        vector tiles_grid_new(400, vector(400, false));
+        for (int y = 1; y < tiles_grid.size() - 1; y++) {
+            for (int x = 1; x < tiles_grid.size() - 1; x++) {
+                cout << "test"; // TODO finish this
+            }
+        }
     }
 
     cout << "Part 1: " << part_1 << endl;
