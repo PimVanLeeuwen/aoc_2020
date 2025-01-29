d9978bfaba563ea66612a6da95c5f5ab5da7f1d5
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-29 17:34:45 +0100

    Completed day 24

--- day_24/Day24.cpp
+++ day_24/Day24.cpp
@@ -20,25 +20,17 @@ void Day24::execute(const vector<string> &lines) {
         int i = 0;
         while (i < line.size()) {
             if (line[i] == 'e') {
-                tile.first++;
-                i++;
+                tile.first++; i++;
             } else if (line[i] == 'w') {
-                tile.first--;
-                i++;
+                tile.first--; i++;
             } else if (line[i] == 's' && line[i + 1] == 'w') {
-                tile.first--;
-                tile.second++;
-                i += 2;
+                tile.first--; tile.second++; i += 2;
             } else if (line[i] == 'n' && line[i + 1] == 'e') {
-                tile.first++;
-                tile.second--;
-                i += 2;
+                tile.first++; tile.second--; i += 2;
             } else if (line[i] == 's' && line[i + 1] == 'e') {
-                tile.second++;
-                i += 2;
+                tile.second++; i += 2;
             } else if (line[i] == 'n' && line[i + 1] == 'w') {
-                tile.second--;
-                i += 2;
+                tile.second--; i += 2;
             }
         }
         tiles_grid[tile.second][tile.first] = tiles_grid[tile.second][tile.first] ? false : true;
@@ -54,8 +46,23 @@ void Day24::execute(const vector<string> &lines) {
         vector tiles_grid_new(400, vector(400, false));
         for (int y = 1; y < tiles_grid.size() - 1; y++) {
             for (int x = 1; x < tiles_grid.size() - 1; x++) {
-                cout << "test"; // TODO finish this
+                int count_black = 0;
+                for (const auto [x_dis, y_dis] : vector<pair<int, int>>({{1,0}, {-1,0}, {0,1}, {0,-1}, {1,-1}, {-1, 1}})) {
+                    if (tiles_grid[y+y_dis][x+x_dis]) count_black++;
+                }
+
+                if (tiles_grid[y][x] && (count_black == 0 || count_black > 2)) tiles_grid_new[y][x] = false;
+                else if (!tiles_grid[y][x] && count_black == 2) tiles_grid_new[y][x] = true;
+                else tiles_grid_new[y][x] = tiles_grid[y][x];
             }
+        }
+
+        tiles_grid = tiles_grid_new;
+    }
+
+    for (int y = 0; y < tiles_grid.size(); y++) {
+        for (int x = 0; x < tiles_grid.size(); x++) {
+            if (tiles_grid[y][x]) part_2++;
         }
     }
 
