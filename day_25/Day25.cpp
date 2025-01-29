9bdb2df1a90a2bb9bef3dcaf706f2718be318a1f
Pim van Leeuwen <git@pvleeuwen.com>
2025-01-29 18:06:33 +0100

    Completed day 25 and hence also done with Advent of Code 2020

--- day_25/Day25.cpp
+++ day_25/Day25.cpp
@@ -8,21 +8,30 @@
 
 using namespace std;
 
+long transform(const long value, const long subject_nr) {
+    return value * subject_nr % 20201227;
+}
+
 void Day25::execute(const vector<string> &lines) {
-    long part_1 = 0, part_2 = 0;
-    vector<vector<string>> matrix;
+    const pair<long, long> pk = {9232416, 14144084};
+    tuple<long, long, long> n_pk = {1, 1, 1};
+    pair<long, long> loop = {0, 0};
 
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line); string token;
-        vector<string> matrix_line;
+    constexpr long subject = 7;
 
-        // separate line
-        while (getline(ss, token, ' ')) matrix_line.push_back(token);
-
-        matrix.push_back(matrix_line);
+    while (get<0>(n_pk) != pk.first) {
+        get<0>(n_pk) = transform(get<0>(n_pk), subject);
+        loop.first++;
     }
 
-    cout << "Part 1: " << part_1 << endl;
-    cout << "Part 2: " << part_2 << endl;
+    while (get<1>(n_pk) != pk.second) {
+        get<1>(n_pk) = transform(get<1>(n_pk), subject);
+        loop.second++;
+    }
+
+    for (long i = 0; i < loop.first; i++) {
+        get<2>(n_pk) = transform(get<1>(n_pk), get<2>(n_pk));
+    }
+
+    cout << "Part 1: " << get<2>(n_pk);
 }
